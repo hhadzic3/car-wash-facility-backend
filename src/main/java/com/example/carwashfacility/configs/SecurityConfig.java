7db6b00521708e48450d564982fa84e884f9343d
Harun Hadzic <48292553+hhadzic3@users.noreply.github.com>
2023-01-13 22:18:07 +0100

    Security routes fixed

--- src/main/java/com/example/carwashfacility/configs/SecurityConfig.java
+++ src/main/java/com/example/carwashfacility/configs/SecurityConfig.java
@@ -44,7 +44,6 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/**/auth/**").permitAll()
                 .antMatchers("/**/register").permitAll()
-                .antMatchers("/**/user").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
